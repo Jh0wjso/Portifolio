082ca4fd3d10f1c2a1ff9ec6d6f6f37b40927b70
JhoJSO <jhonatansilverio1258@gmail.com>
2022-07-20 18:24:03 -0300

    First website prototype

---
+++ portifolio/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Profile from './components/Home/Profile';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Profile/>
+    </div>
+  );
+}
+
+export default App;
