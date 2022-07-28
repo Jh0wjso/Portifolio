b45cb224cfe0737e0f2d8ed50df8b0bbbe162d9a
JhoJSO <jhonatansilverio1258@gmail.com>
2022-07-28 20:10:46 -0300

    Updates on footer, navbar amd profile.

--- portifolio/src/App.js
+++ portifolio/src/App.js
@@ -3,12 +3,36 @@ import Profile from './components/Home/Profile';
 import AboutMe from './components/AboutMe/AboutMe';
 import Footer from './components/Footer/Footer';
 
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
 function App() {
   return (
-    <div className="MainContent">
-      <Profile/>
-      <AboutMe/>
-      <Footer/>
+    <div className="MainContent h-100">
+      <div className='d-flex justify-content-center w-screen container'>
+        <Navbar collapseOnSelect expand="lg" bg="#383737" variant="dark">
+          <Container>
+            <Navbar.Brand href="App.js">Jhonatan Silvério</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#profile">Sobre mim</Nav.Link>
+                <Nav.Link href="#pricing">Contato</Nav.Link>
+              </Nav>
+              <Nav>
+                <Nav.Link href="#deets">Curriculo</Nav.Link>
+                <Nav.Link eventKey={2} href="#memes">
+                  Projetos
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
+      <div className='w-100'><Profile/></div>
+      <div id='profile' className='w-100'><AboutMe/></div>
+      <div id='footer'><Footer/></div>
     </div>
   );
 }
--- portifolio/src/components/Footer/Footer.js
+++ portifolio/src/components/Footer/Footer.js
@@ -2,6 +2,15 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <div/>
+    <div>
+      <footer class="my-5 pt-5 text-muted text-center text-small">
+        <p class="mb-1">&copy; 2017–2021 Company Name</p>
+        <ul class="list-inline">
+          <li class="list-inline-item"><a href="#">Privacy</a></li>
+          <li class="list-inline-item"><a href="#">Terms</a></li>
+          <li class="list-inline-item"><a href="#">Support</a></li>
+        </ul>
+      </footer>
+    </div>
   )
 }
