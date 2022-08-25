4ba5f895550a3e912523e1d635dea02a907d6f5d
Jh0wjso <jhonatansilverio1258@gmail.com>
2022-08-25 10:46:46 -0300

    Updates

---
+++ precache-manifest.deb09abee9efb164d2074e0f5f8c1bd3.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "6324296637d9578d8d82",
+    "url": "/Portifolio/static/css/main.ba101c6d.chunk.css"
+  },
+  {
+    "revision": "6324296637d9578d8d82",
+    "url": "/Portifolio/static/js/main.63242966.chunk.js"
+  },
+  {
+    "revision": "aea4a5bdfab3b8beece5",
+    "url": "/Portifolio/static/js/1.aea4a5bd.chunk.js"
+  },
+  {
+    "revision": "533b629858af7495e169",
+    "url": "/Portifolio/static/js/2.533b6298.chunk.js"
+  },
+  {
+    "revision": "4cac1b611fe0ad412e33",
+    "url": "/Portifolio/static/js/runtime~main.4cac1b61.js"
+  },
+  {
+    "revision": "93475b93fdb08e84eacbf2d151798bf4",
+    "url": "/Portifolio/static/media/watchball.93475b93.png"
+  },
+  {
+    "revision": "b39996f102e4885ebb027f1d12e69b60",
+    "url": "/Portifolio/static/media/portifolio.b39996f1.png"
+  },
+  {
+    "revision": "ac72c0619f87725a518c94078f87112e",
+    "url": "/Portifolio/static/media/styudyplatform.ac72c061.png"
+  },
+  {
+    "revision": "c55fa2685cbbeaf1cb05a00a6bb1a4c7",
+    "url": "/Portifolio/static/media/BMI_Calculator.c55fa268.jpeg"
+  },
+  {
+    "revision": "22e301ff8c237d531250948b4130dcaf",
+    "url": "/Portifolio/static/media/CoinCotation.22e301ff.jpeg"
+  },
+  {
+    "revision": "2064a60f6e23197bff90e748e017633b",
+    "url": "/Portifolio/static/media/Stylos.2064a60f.png"
+  },
+  {
+    "revision": "f61a4eb27371b7453bf5b12ab3648b9e",
+    "url": "/Portifolio/static/media/Poppins-Medium.f61a4eb2.ttf"
+  },
+  {
+    "revision": "a3e0b5f427803a187c1b62c5919196aa",
+    "url": "/Portifolio/static/media/Poppins-Bold.a3e0b5f4.ttf"
+  },
+  {
+    "revision": "8b6af8e5e8324edfd77af8b3b35d7f9c",
+    "url": "/Portifolio/static/media/Poppins-Regular.8b6af8e5.ttf"
+  },
+  {
+    "revision": "4cdacb8f89d588d69e8570edcbe49507",
+    "url": "/Portifolio/static/media/Poppins-SemiBold.4cdacb8f.ttf"
+  },
+  {
+    "revision": "544fa4f2678a8285eb88b8dfe503c90c",
+    "url": "/Portifolio/static/media/Poppins-ExtraBold.544fa4f2.ttf"
+  },
+  {
+    "revision": "f6ea751e936ade6edcd03a26b8153b4a",
+    "url": "/Portifolio/static/media/Poppins-Light.f6ea751e.ttf"
+  },
+  {
+    "revision": "25cd0f688f815bc4f6ac2b71eb6278ba",
+    "url": "/Portifolio/static/media/Poppins-Thin.25cd0f68.ttf"
+  },
+  {
+    "revision": "077d635ffaa847c794787d0c753f0deb",
+    "url": "/Portifolio/static/media/bg.077d635f.jpg"
+  },
+  {
+    "revision": "0ef42eccc3f2f34df307b98da0b37f20",
+    "url": "/Portifolio/static/media/profilephoto.0ef42ecc.jpg"
+  },
+  {
+    "revision": "39fe07e7ff484c9ed3276dc845fa2905",
+    "url": "/Portifolio/static/media/profilepicture.39fe07e7.jpg"
+  },
+  {
+    "revision": "f413668b0d5f4bfb569657f9c1b113ec",
+    "url": "/Portifolio/index.html"
+  }
+];
