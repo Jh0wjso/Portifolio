c795291dda2e75e0d2c16ff32bfd6e5cf24ed1d3
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-02-12 19:53:18 -0300

    Deleting old project

--- portifolio/src/components/Footer/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Footer() {
-  return (
-    <footer class="mt-5 mb-0 w-100 text-center text-small">
-      <p>&copy; Jhonatan Silvério</p>
-    </footer>
-  )
-}
