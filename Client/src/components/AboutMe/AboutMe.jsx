10371c76115294150d30f77868656dc35964f7a6
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-08-30 22:38:14 -0300

    About me text update

--- Client/src/components/AboutMe/AboutMe.jsx
+++ Client/src/components/AboutMe/AboutMe.jsx
@@ -11,12 +11,15 @@ export default function AboutMe() {
 
         <div className="whoAmI">
           <strong className="highlight-text text-center">Sobre mim</strong>
+          <strong>Oi, como vai?</strong>
           <p className="aboutMeText">
-            Atualmente Cursando Ciência da Computação na Universidade Estadual
-            do Norte do Paraná(UENP) no campus Luiz Meneghel. Estou na busca de
-            me tornar um dev de alto nível, no momento estou estudando ReactJs,
-            React Native, NodeJs, JavaScript, MongoDb, Python, Java,
-            PostgresSQL, entre outras tecnologias.
+            É ótimo ver você aqui! Desde 2019, venho estudando e desenvolvendo software enquanto trabalho em aplicações web para diversos setores. 
+            Meu foco é oferecer soluções inovadoras e eficientes para empresas que desejam ter presença online. 
+            Também desenvolvi aplicações web para lojas físicas para ampliar seu alcance e oferecer uma experiência de compra integrada aos seus clientes. 
+            Atualmente estou cursando Ciência da Computação e criei quatro aplicativos mobile para ampliar meu portfólio e aprofundar meus conhecimentos. 
+            Minha experiência me proporcionou amplo conhecimento em programação web e mobile, design de interface e experiência do usuário, 
+            além de uma visão atualizada do mercado de tecnologia. Estou sempre em busca de novos desafios e oportunidades para me desenvolver ainda 
+            mais neste campo fascinante e em constante evolução.
           </p>
         </div>
       </div>
