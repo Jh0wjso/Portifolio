c7cd62f017e37786bdc2c3d3dcdc5f9636718795
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-10-02 22:04:01 -0300

    Projects data

---
+++ Client/src/data/Projects.data.ts
@@ -0,0 +1,66 @@
+type TechnologiesType = {
+    projectState: string,
+    tittle: string,
+    image: string,
+    projectLink: string,
+    projectType: string
+}
+
+export const ProjectData:TechnologiesType[] = [
+    {
+        projectState: "Finalizado",
+        tittle: "WatchBall",
+        image: "../../assets/Home/Wb.png",
+        projectLink: "https://watchball.vercel.app/",
+        projectType: "Site"
+    },
+    {
+        projectState: "Em Produção",
+        tittle: "CoinCotation",
+        image: "../../assets/Home/CoinCotation.jpeg",
+        projectLink: "https://github.com/Jh0wjso/Coin-Cotation-App",
+        projectType: "Repositório"
+    },
+    {
+        projectState: "Em Produção",
+        tittle: "Web Store",
+        image: "../../assets/Home/Stylos.png",
+        projectLink: "https://github.com/Jh0wjso/Web_Store",
+        projectType: "Repositório"
+    },
+    {
+        projectState: "Em Produção",
+        tittle: "WannabeHealth",
+        image: "../../assets/Home/Logo2.png",
+        projectLink: "",
+        projectType: "EM BREVE"
+    },
+    {
+        projectState: "Finalizado",
+        tittle: "BMI-Calc",
+        image: "../../assets/Home/BMI-Calc.png",
+        projectLink: "https://github.com/Jh0wjso/JSO_HEALTH",
+        projectType: "Repositório"
+    },
+    {
+        projectState: "Em Produção",
+        tittle: "OnMaps",
+        image: "../../assets/Home/onmaps.png",
+        projectLink: "",
+        projectType: "Em Breve"
+    },
+    {
+        projectState: "Finalizado",
+        tittle: "Agrolugue",
+        image: "../../assets/Home/agrolugue.png",
+        projectLink: "https://github.com/Jh0wjso/Geniuscon2022",
+        projectType: "Repositório"
+    },
+    {
+        projectState: "Em Breve",
+        tittle: "X-Fut",
+        image: "../../assets/Home/iohub.jpg",
+        projectLink: "https://www.instagram.com/iohub.tech/",
+        projectType: "Conheça"
+    },
+]
