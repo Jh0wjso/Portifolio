3fb3a96c6769d2b90a48b49bbb6790afee31b6fc
Jh0wjso <jhonatansilverio1258@gmail.com>
2024-05-01 18:43:48 -0300

    Profile Update

--- Client/src/components/Home/Profile.jsx
+++ Client/src/components/Home/Profile.jsx
@@ -7,64 +7,29 @@ export default function Profile() {
       <div className="profile-container">
         <div className="profile-parent">
           <div className="profile-details">
-            <div className="colz d-flex justify-content-center">
-              <div className="colz-icon">
-                <a
-                  href="https://www.linkedin.com/in/jhonatansilverio/"
-                  target={"_blank"}
-                >
-                  <i className="fa fa-linkedin-square text-white mr-4" />
-                </a>
-                <a
-                  href="https://github.com/Jh0wjso"
-                  target={"_blank"}
-                >
-                  <i className="fa fa-github-square text-white mr-4" />
-                </a>
-                <a
-                  href="https://www.instagram.com/jhojso/"
-                  target={"_blank"}
-                >
-                  <i className="fa fa-instagram text-white mr-4" />
-                </a>
-              </div>
-            </div>
-
             <div className="profile-details-name">
-              <span className="primary-text">
-                {" "}
-                Opá, Tudo certo? Espero que sim! Prazer me chamo{" "}
-                <span className="highlighted-text">Jhonatan</span>!
+              <span className="primary-text h4">
+                Hello, i am <br />
               </span>
+              <h1 className="highlighted-text"> {"<"} Jhonatan <br /> Silverio {"/>"} </h1>
             </div>
 
             <div className="profile-details-role p-3">
               <span className="primary-text">
-                {" "}
                 <h1>
-                  {"Desenvolvedor Front-end"}
+                  {"Front-end Developer"}
                 </h1>
-                <span className="profile-role-tagline font-weight-bold">
-                  Habilidades em front e back end.
+                <section className="d-flex">
+                <span className="d-flex">
+                  <h1 className="h1">+02</h1>
+                  <span className="w-50 ml-1" style={{color: "#b1b1b1", fontWeight: "bold"}}>years of experience </span>
                 </span>
+                <span className="d-flex">
+                  <h1 className="h1">+10</h1>
+                  <span className="w-50 ml-1" style={{color: "#b1b1b1", fontWeight: "bold"}}>projects completed</span>
+                </span>
+                </section>
               </span>
-            </div>
-            <div className="profile-options container">
-              <a
-                href="https://wa.me/5514997981721"
-                target={"_blank"}
-              >
-                <button className="btn primary-btn mb-1">
-                  {""}
-                  Me Contate{" "}
-                </button>
-              </a>
-
-              <a href="Curriculo.pdf" download="Jhonatan Silverio.pdf">
-                <button className="btn highlighted-btn">
-                  Baixar Currículo
-                </button>
-              </a>
             </div>
           </div>
           <div className="profile-picture">
