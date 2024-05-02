31cfc7c1fff7286dca01cc3798d9c8eadb7d40fa
Jh0wjso <jhonatansilverio1258@gmail.com>
2024-05-02 15:36:44 -0300

    About Me finished

--- Client/src/components/AboutMe/AboutMe.jsx
+++ Client/src/components/AboutMe/AboutMe.jsx
@@ -1,26 +1,46 @@
 import React from "react";
+import { IoLogoJavascript } from "react-icons/io5";
 import "./AboutMe.css";
+import { HiCodeBracket } from "react-icons/hi2";
+import { RiBracesLine } from "react-icons/ri";
+import { PiBracketsRoundBold } from "react-icons/pi";
 
 export default function AboutMe() {
   return (
     <main className="aboutMe" id="#aboutMe">
       <div className="aboutMeContent">
-        <div className="my-picture">
-          <div className="picture-background"></div>
-        </div>
-
         <div className="whoAmI">
-          <strong className="highlight-text text-center">Sobre mim</strong>
-          <strong>Oi, como vai?</strong>
+          <strong className="highlight-text text-center">About Me</strong>
+          <div className="d-flex">
+            <strong className="highlighted-text h3">aboutMe</strong>
+            <strong className="h3"><PiBracketsRoundBold /></strong>
+          </div>
           <p className="aboutMeText">
-            É ótimo ver você aqui! Desde 2019, venho estudando e desenvolvendo software enquanto trabalho em aplicações web para diversos setores. 
-            Meu foco é oferecer soluções inovadoras e eficientes para empresas que desejam ter presença online. 
-            Também desenvolvi aplicações web para lojas físicas para ampliar seu alcance e oferecer uma experiência de compra integrada aos seus clientes. 
-            Atualmente estou cursando Ciência da Computação e criei quatro aplicativos mobile para ampliar meu portfólio e aprofundar meus conhecimentos. 
-            Minha experiência me proporcionou amplo conhecimento em programação web e mobile, design de interface e experiência do usuário, 
-            além de uma visão atualizada do mercado de tecnologia. Estou sempre em busca de novos desafios e oportunidades para me desenvolver ainda 
-            mais neste campo fascinante e em constante evolução.
+            I have been studying software development since 2019 and working as
+            a freelancer for over a year, with web applications I have already
+            developed some web applications for physical stores that did not
+            have WEB applications, and Landing Pages for other businesses and
+            always concerned about the user experience and using data metrics to
+            achieve high retention rates I have already created four mobile
+            applications, my specialties are React, React Native, JavaScript,
+            TailwindCSS, NextJs, NestJS, PHP.
           </p>
+        </div>
+        <div className="my-picture">
+          <section className="card">
+            <div className="d-flex flex-column mr-5">
+              <strong>Front-end Developer</strong>
+              <a href="#projects" className="linkCard">Projects</a>
+            </div>
+            <HiCodeBracket  size={50} />
+          </section>
+          <section className="card">
+            <div className="d-flex flex-column mr-5">
+              <strong>Contact me</strong>
+              <a href="#contact" className="linkCard">Hire me!</a>
+            </div>
+            <RiBracesLine size={50} />
+          </section>
         </div>
       </div>
     </main>
