296bbe13777874681e28a82407ffa67580975ab2
Jh0wjso <jhonatansilverio1258@gmail.com>
2024-05-04 10:47:21 -0300

    Translate projects components to English

--- Client/src/components/Projects/Projects.jsx
+++ Client/src/components/Projects/Projects.jsx
@@ -7,16 +7,21 @@ import WbH from "../../assets/Home/Logo2.png";
 import BMI_Calculator from "../../assets/Home/BMI-Calc.png";
 import CoinCotation from "../../assets/Home/CoinCotation.jpeg";
 import Stylos from "../../assets/Home/Stylos.png";
-import Ball from "../../assets/Home/iohub.jpg";
-import OnMaps from "../../assets/Home/onmaps.png";
+
+import { PiBracketsRoundBold } from "react-icons/pi";
 
 export default function Projects() {
   return (
-    <section className="mainContent container d-flex justify-content-center">
-      <h1 className="font-weight-bold highlight-text text-center">Projetos</h1>
+    <section className="mainContent container d-flex justify-content-center p-5">
+      <div className="d-flex">
+        <strong className="highlighted-text h3">projects</strong>
+        <strong className="h3">
+          <PiBracketsRoundBold />
+        </strong>
+      </div>
       <div className="projects d-flex justify-content-center">
         <div className="card-project">
-          <strong className="logo">Finalizado</strong>
+          <strong className="logo">Finished</strong>
           <h2 className="text-center">WatchBall</h2>
           <img src={watchball} alt="watchball-logo" className="project" />
           <div className="button-right">
@@ -26,7 +31,7 @@ export default function Projects() {
           </div>
         </div>
         <div className="card-project">
-          <strong className="logo">Em Produção</strong>
+          <strong className="logo">In Building</strong>
           <h2 className="title">CoinCotation</h2>
           <img src={CoinCotation} alt="coinCotation-logo" className="project" />
           <div className="button-right">
@@ -34,32 +39,32 @@ export default function Projects() {
               href="https://github.com/Jh0wjso/Coin-Cotation-App"
               target={"_blank"}
             >
-              Repositório
+              Repository
             </a>
           </div>
         </div>
         <div className="card-project">
-          <strong className="logo">Finalizado</strong>
+          <strong className="logo">Finished</strong>
           <h2 className="title">Web Store</h2>
           <img src={Stylos} alt="Stylos-logo" className="project" />
           <div className="button-right">
             <a href="https://github.com/Jh0wjso/Web_Store" target={"_blank"}>
-              Repositório
+              Repository
             </a>
           </div>
         </div>
       </div>
       <div className="projects d-flex justify-content-center">
         <div className="card-project">
-          <strong className="logo">Em Produção</strong>
+          <strong className="logo">In Building</strong>
           <h2 className="title">WannabeHealth</h2>
           <img src={WbH} alt="study-platform-logo" className="project" />
           <div className="button-right">
-            <a onClick={() => {}}>EM BREVE</a>
+            <a href="https://github.com/Jh0wjso/WbH" target="_blank">In coming soon</a>
           </div>
         </div>
         <div className="card-project">
-          <strong className="logo">Finalizado</strong>
+          <strong className="logo">Finished</strong>
           <h2 className="title">BMI-Calc</h2>
           <img
             src={BMI_Calculator}
@@ -68,12 +73,12 @@ export default function Projects() {
           />
           <div className="button-right">
             <a href="https://github.com/Jh0wjso/JSO_HEALTH" target={"_blank"}>
-              Repositório
+              Repository
             </a>
           </div>
         </div>
         <div className="card-project">
-          <strong className="logo">Finalizado</strong>
+          <strong className="logo">Finished</strong>
           <h2 className="title">Agrolugue</h2>
           <img src={Agrolugue} alt="Agrolugue-logo" className="project" />
           <div className="button-right">
@@ -81,7 +86,7 @@ export default function Projects() {
               href="https://github.com/Jh0wjso/Geniuscon2022"
               target={"_blank"}
             >
-              Repositório
+              Repository
             </a>
           </div>
         </div>
