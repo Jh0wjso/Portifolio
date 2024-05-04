1afa99e71428f13580ae31dccd9230763b52c395
Jh0wjso <jhonatansilverio1258@gmail.com>
2024-05-04 10:12:37 -0300

    Technologies Update

--- Client/src/components/Technologies/Technologies.jsx
+++ Client/src/components/Technologies/Technologies.jsx
@@ -1,29 +1,25 @@
 import { TechnologiesData } from "../../data/Technologies.data";
 import "./Technologies.css";
 import React from "react";
+import { PiBracketsRoundBold } from "react-icons/pi";
 
 export default function Technologies() {
   return (
-    <section className="content-background container d-flex flex-column">
-      <h1 className="font-weight-bold highlight-text text-center">
-        Competências.
-      </h1>
+    <section className="content-background d-flex flex-column">
+      <div className="d-flex">
+        <strong className="highlighted-text h3">skills</strong>
+        <strong className="h3">
+          <PiBracketsRoundBold />
+        </strong>
+      </div>
       <div className="d-flex flex-wrap justify-content-center">
         {TechnologiesData.map((item) => (
-          <div className="w-25 item-tec text-center d-flex flex-column justify-content-center align-items-center">
-            <svg
-              className="p-2 mb-1 mt-1"
-              xmlns="http://www.w3.org/2000/svg"
-              width="60"
-              height="60"
-              fill="currentColor"
-              class="bi bi-filetype-html"
-              viewBox="0 0 16 16"
-            >
-              <path d={item.iconPath} />
-            </svg>
-            <strong>{item.tittle}</strong>
-            <small>{item.subtitle}</small>
+          <div className="w-25 item-tec1 d-flex justify-content-center align-items-center">
+            <img
+              src={item.iconPath}
+              alt="JavaScript"
+              width={75}
+            />
           </div>
         ))}
       </div>
--- Client/src/data/Technologies.data.ts
+++ Client/src/data/Technologies.data.ts
@@ -1,48 +1,42 @@
 type TechnologiesType = {
     iconPath: string,
-    tittle: string,
-    subtitle: string
 }
 
 export const TechnologiesData:TechnologiesType[] = [
     {
-        iconPath: "M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z",
-        tittle: "Desenvolvedor de Apps",
-        subtitle: "Desenvolvimento de Aplicativos para Android e IOS, com React Native, Android Studio e Flutter."
+        iconPath: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/600px-JavaScript-logo.png",
     },
     {
-        iconPath: "M14 4.5V11h-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5L14 4.5Zm-9.736 7.35v3.999h-.791v-1.714H1.79v1.714H1V11.85h.791v1.626h1.682V11.85h.79Zm2.251.662v3.337h-.794v-3.337H4.588v-.662h3.064v.662H6.515Zm2.176 3.337v-2.66h.038l.952 2.159h.516l.946-2.16h.038v2.661h.715V11.85h-.8l-1.14 2.596H9.93L8.79 11.85h-.805v3.999h.706Zm4.71-.674h1.696v.674H12.61V11.85h.79v3.325Z",
-        tittle: "Criação de Sites",
-        subtitle: "Criação de Sites com HTML, CSS, JS, PHP."
+        iconPath: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/2048px-Typescript_logo_2020.svg.png",
     },
     {
-        iconPath: "M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.371 2.371 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976l2.61-3.045zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0zM1.5 8.5A.5.5 0 0 1 2 9v6h1v-5a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1v5h6V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5zM4 15h3v-5H4v5zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3zm3 0h-2v3h2v-3z",
-        tittle: "Criação de Loja Virtual",
-        subtitle: "Atualmente estudando sobre a criação de lojas virtuais com pagamentos através da Internet."
+        iconPath: "https://www.w3.org/html/logo/downloads/HTML5_Badge_512.png",
     },
     {
-        iconPath: "M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383zm.653.757c-.757.653-1.153 1.44-1.153 2.056v.448l-.445.049C2.064 6.805 1 7.952 1 9.318 1 10.785 2.23 12 3.781 12h8.906C13.98 12 15 10.988 15 9.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 4.825 10.328 3 8 3a4.53 4.53 0 0 0-2.941 1.1z",
-        tittle: "DEVOPS",
-        subtitle: "Me especializando em AWS."
+        iconPath: "https://cdn.iconscout.com/icon/free/png-256/free-css-131-722685.png",
     },
     {
-        iconPath: "M3 3.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0Zm1.5 0a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0Zm1 .5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1Z M.5 1a.5.5 0 0 0-.5.5v13a.5.5 0 0 0 .5.5h15a.5.5 0 0 0 .5-.5v-13a.5.5 0 0 0-.5-.5H.5ZM1 5V2h14v3H1Zm0 1h14v8H1V6Z",
-        tittle: "Desenvolvedor FrontEnd",
-        subtitle: "Me especializando em ReactJs, HTML5, CSS, BootStrap, Vue.js, ..."
+        iconPath: "https://cdn1.iconfinder.com/data/icons/programing-development-8/24/react_logo-512.png",
     },
     {
-        iconPath: "M13.5 3a.5.5 0 0 1 .5.5V11H2V3.5a.5.5 0 0 1 .5-.5h11zm-11-1A1.5 1.5 0 0 0 1 3.5V12h14V3.5A1.5 1.5 0 0 0 13.5 2h-11zM0 12.5h16a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 12.5z",
-        tittle: "Aplicações Desktop",
-        subtitle: "Estudando Java, C# .NET"
+        iconPath: "https://static-00.iconduck.com/assets.00/node-js-icon-454x512-nztofx17.png",
     },
     {
-        iconPath: "M12.633 7.653c0-.848-.305-1.435-.566-1.892l-.08-.13c-.317-.51-.594-.958-.594-1.48 0-.63.478-1.218 1.152-1.218.02 0 .039.002.058.003l.031.003A6.838 6.838 0 0 0 8 1.137 6.855 6.855 0 0 0 2.266 4.23c.16.005.313.009.442.009.717 0 1.828-.087 1.828-.087.37-.022.414.521.044.565 0 0-.371.044-.785.065l2.5 7.434 1.5-4.506-1.07-2.929c-.369-.022-.719-.065-.719-.065-.37-.022-.326-.588.043-.566 0 0 1.134.087 1.808.087.718 0 1.83-.087 1.83-.087.37-.022.413.522.043.566 0 0-.372.043-.785.065l2.48 7.377.684-2.287.054-.173c.27-.86.469-1.495.469-2.046zM1.137 8a6.864 6.864 0 0 0 3.868 6.176L1.73 5.206A6.837 6.837 0 0 0 1.137 8z M6.061 14.583 8.121 8.6l2.109 5.78c.014.033.03.064.049.094a6.854 6.854 0 0 1-4.218.109zm7.96-9.876c.03.219.047.453.047.706 0 .696-.13 1.479-.522 2.458l-2.096 6.06a6.86 6.86 0 0 0 2.572-9.224z M0 8c0-4.411 3.589-8 8-8 4.41 0 8 3.589 8 8s-3.59 8-8 8c-4.411 0-8-3.589-8-8zm.367 0c0 4.209 3.424 7.633 7.633 7.633 4.208 0 7.632-3.424 7.632-7.633C15.632 3.79 12.208.367 8 .367 3.79.367.367 3.79.367 8z",
-        tittle: "WordPress",
-        subtitle: "Também crio sites com a ferramenta WordPress."
+        iconPath: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWUlQgefawODpvCN7U-fsf5dT63uMY5udZ5F9Ewmjbmg&s",
     },
     {
-        iconPath: "M14 4.5V14a2 2 0 0 1-2 2v-1a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5L14 4.5ZM0 14.841a1.129 1.129 0 0 0 .401.823c.13.108.288.192.478.252s.411.091.665.091c.338 0 .624-.053.858-.158.237-.106.416-.252.54-.44a1.17 1.17 0 0 0 .187-.656c0-.224-.045-.41-.135-.56a1 1 0 0 0-.375-.357 2.027 2.027 0 0 0-.565-.21l-.621-.144a.97.97 0 0 1-.405-.176.369.369 0 0 1-.143-.299c0-.156.061-.284.184-.384.125-.101.296-.152.513-.152.143 0 .266.022.37.068a.624.624 0 0 1 .245.181.56.56 0 0 1 .12.258h.75a1.092 1.092 0 0 0-.199-.566 1.21 1.21 0 0 0-.5-.41 1.813 1.813 0 0 0-.78-.152c-.293 0-.552.05-.776.15-.225.099-.4.24-.528.421-.127.182-.19.395-.19.639 0 .201.04.376.123.524.082.149.199.27.351.367.153.095.332.167.54.213l.618.144c.207.049.36.113.462.193a.387.387 0 0 1 .153.325c0 .11-.029.207-.085.29A.558.558 0 0 1 2 15.31c-.111.047-.249.07-.413.07-.117 0-.224-.013-.32-.04a.835.835 0 0 1-.248-.115.579.579 0 0 1-.255-.384H0Zm6.878 1.489-.507-.739c.176-.162.31-.362.401-.6.092-.239.138-.507.138-.806v-.501c0-.371-.07-.693-.208-.967a1.495 1.495 0 0 0-.589-.636c-.256-.15-.561-.225-.917-.225-.351 0-.656.075-.914.225-.256.149-.453.36-.592.636a2.138 2.138 0 0 0-.205.967v.5c0 .37.069.691.205.965.139.273.336.485.592.636a1.8 1.8 0 0 0 .914.222 1.8 1.8 0 0 0 .6-.1l.294.422h.788ZM4.262 14.2v-.522c0-.246.038-.456.114-.63a.91.91 0 0 1 .325-.398.885.885 0 0 1 .495-.138c.192 0 .357.046.495.138a.88.88 0 0 1 .325.398c.077.174.115.384.115.63v.522c0 .164-.018.312-.053.445-.035.13-.087.244-.155.34l-.106-.14-.105-.147h-.733l.451.65a.638.638 0 0 1-.251.047.872.872 0 0 1-.487-.147.916.916 0 0 1-.32-.404 1.67 1.67 0 0 1-.11-.644Zm3.986 1.057h1.696v.674H7.457v-3.999h.79v3.325Z",
-        tittle: "Bancos de Dados",
-        subtitle: "Me especializando em MySql, PostgresSQL, MongoDB ..."
+        iconPath: "https://seeklogo.com/images/N/nestjs-logo-09342F76C0-seeklogo.com.png",
+    },
+    {
+        iconPath: "https://pngimg.com/d/linux_PNG1.png",
+    },
+    {
+        iconPath: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1869px-Python-logo-notext.svg.png",
+    },
+    {
+        iconPath: "https://seeklogo.com/images/A/azure-sql-database-logo-D7A32C9CD9-seeklogo.com.png",
+    },
+    {
+        iconPath: "https://download.logo.wine/logo/Amazon_Web_Services/Amazon_Web_Services-Logo.wine.png",
     },
 ];
