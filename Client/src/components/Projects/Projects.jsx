56049972a0e59fd89d0083e78dc76f806c5797bf
Jh0wjso <jhonatansilverio1258@gmail.com>
2025-07-30 20:44:30 -0300

    Update experiences

--- Client/src/components/Projects/Projects.jsx
+++ Client/src/components/Projects/Projects.jsx
@@ -3,10 +3,9 @@ import "./Projects.css";
 
 import watchball from "../../assets/Home/Wb.png";
 import Agrolugue from "../../assets/Home/agrolugue.png";
-import WbH from "../../assets/Home/Logo2.png";
-import BMI_Calculator from "../../assets/Home/BMI-Calc.png";
-import CoinCotation from "../../assets/Home/logo.png";
-import Stylos from "../../assets/Home/Stylos.png";
+import Vtmh from "../../assets/Home/vtmh.png";
+import GameSessionGG from "../../assets/Home/play.png";
+import operately from "../../assets/Home/operately.png";
 
 import { PiBracketsRoundBold } from "react-icons/pi";
 
@@ -14,10 +13,46 @@ export default function Projects() {
   return (
     <section className="mainContent d-flex justify-content-center">
       <div className="d-flex">
-        <strong className="highlighted-text h3">personal_projects</strong>
+        <strong className="highlighted-text h3">professional_projects</strong>
         <strong className="h3">
           <PiBracketsRoundBold />
         </strong>
+      </div>
+      <div className="projects d-flex justify-content-center">
+        <div className="card-project">
+          <strong className="logo">Ongoing</strong>
+          <h2 className="title">GameSession.gg</h2>
+          <img
+            src={GameSessionGG}
+            alt="BMI-Calculator-logo"
+            className="project"
+          />
+          <div className="button-right">
+            <a href="https://www.gamesession.gg/" target={"_blank"}>
+              Website
+            </a>
+          </div>
+        </div>
+        <div className="card-project">
+          <strong className="logo">Contribution</strong>
+          <h2 className="title">Operately</h2>
+          <img src={operately} alt="coinCotation-logo" className="project" />
+          <div className="button-right">
+            <a href="https://github.com/Jh0wjso/operately" target={"_blank"}>
+              Repository
+            </a>
+          </div>
+        </div>
+        <div className="card-project">
+          <strong className="logo">In coming soon</strong>
+          <h2 className="title">VittaMaisHub</h2>
+          <img src={Vtmh} alt="study-platform-logo" className="project" />
+          <div className="button-right">
+            <a href="https://vtmh-app.vercel.app/" target="_blank">
+              Website
+            </a>
+          </div>
+        </div>
       </div>
       <div className="projects d-flex justify-content-center">
         <div className="card-project">
@@ -26,43 +61,7 @@ export default function Projects() {
           <img src={watchball} alt="watchball-logo" className="project" />
           <div className="button-right">
             <a href="https://watchball.vercel.app/" target={"_blank"}>
-              Site
-            </a>
-          </div>
-        </div>
-        <div className="card-project">
-          <strong className="logo">In Building</strong>
-          <h2 className="title">PremNews</h2>
-          <img src={CoinCotation} alt="coinCotation-logo" className="project" />
-          <div className="button-right">
-            <a href="https://prem-news.vercel.app/" target={"_blank"}>
-              Site
-            </a>
-          </div>
-        </div>
-        <div className="card-project">
-          <strong className="logo">In Building</strong>
-          <h2 className="title">WannabeHealth</h2>
-          <img src={WbH} alt="study-platform-logo" className="project" />
-          <div className="button-right">
-            <a href="https://github.com/Jh0wjso/WbH" target="_blank">
-              In coming soon
-            </a>
-          </div>
-        </div>
-      </div>
-      <div className="projects d-flex justify-content-center">
-        <div className="card-project">
-          <strong className="logo">Finished</strong>
-          <h2 className="title">BMI-Calc</h2>
-          <img
-            src={BMI_Calculator}
-            alt="BMI-Calculator-logo"
-            className="project"
-          />
-          <div className="button-right">
-            <a href="https://github.com/Jh0wjso/JSO_HEALTH" target={"_blank"}>
-              Repository
+              Website
             </a>
           </div>
         </div>
