f3c1bea4d8df32502b24dc872c5bbf674268b348
Jh0wjso <jhonatansilverio1258@gmail.com>
2025-12-11 12:06:24 -0300

    feat: update technologies logo

--- Client/src/data/Technologies.data.ts
+++ Client/src/data/Technologies.data.ts
@@ -19,13 +19,13 @@ export const TechnologiesData:TechnologiesType[] = [
         iconPath: "https://cdn1.iconfinder.com/data/icons/programing-development-8/24/react_logo-512.png",
     },
     {
-        iconPath: "https://static-00.iconduck.com/assets.00/node-js-icon-454x512-nztofx17.png",
+        iconPath: "https://cdn.freebiesupply.com/logos/large/2x/nodejs-1-logo-png-transparent.png",
     },
     {
         iconPath: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWUlQgefawODpvCN7U-fsf5dT63uMY5udZ5F9Ewmjbmg&s",
     },
     {
-        iconPath: "https://seeklogo.com/images/N/nestjs-logo-09342F76C0-seeklogo.com.png",
+        iconPath: "https://upload.wikimedia.org/wikipedia/commons/a/a8/NestJS.svg",
     },
     {
         iconPath: "https://pngimg.com/d/linux_PNG1.png",
@@ -34,7 +34,7 @@ export const TechnologiesData:TechnologiesType[] = [
         iconPath: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1869px-Python-logo-notext.svg.png",
     },
     {
-        iconPath: "https://seeklogo.com/images/A/azure-sql-database-logo-D7A32C9CD9-seeklogo.com.png",
+        iconPath: "https://www.svgrepo.com/show/331760/sql-database-generic.svg",
     },
     {
         iconPath: "https://download.logo.wine/logo/Amazon_Web_Services/Amazon_Web_Services-Logo.wine.png",
